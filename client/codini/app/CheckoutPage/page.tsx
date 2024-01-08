f1e159318e875fc37ef22ef002463aae46d5e31f
louay <louaysakli65@gmail.com>
2024-01-08 18:01:52 +0100

    payment layout footer navbar

---
+++ client/codini/app/CheckoutPage/page.tsx
@@ -0,0 +1,45 @@
+// import type { NextPage } from "next";
+import styles from "./CheckoutPage.module.css";
+
+const CheckoutPage = () => {
+  return (
+    <div className={styles.checkoutPage}>
+    
+     
+      
+      
+      <img className={styles.rectangleIcon} alt="" src="/rectangle@2x.png" />
+      <div className={styles.checkout}>Checkout</div>
+      <div className={styles.summary}>Summary</div>
+      <div className={styles.checkoutPageChild} />
+      <div className={styles.groupDiv}>
+        <div className={styles.groupChild4} />
+        <div className={styles.confirmPayment}>Confirm Payment</div>
+      </div>
+      <img
+        className={styles.confidentTeacherExplainingLIcon}
+        alt=""
+        src="/confidentteacherexplaininglessonpupils-1@2x.png"
+      />
+      <img className={styles.playButtonIcon} alt="" src="/playbutton@2x.png" />
+      <div className={styles.adipisingElitSed}>
+        adipising elit, sed do eiusmod tempor
+      </div>
+      <div className={styles.div1}>$24.69</div>
+      <div className={styles.subtotal}>Subtotal</div>
+      <div className={styles.div2}>$51.38</div>
+      <div className={styles.loremIpsumDollar}>Lorem ipsum dollar...</div>
+      <div className={styles.checkoutPageItem} />
+      <div className={styles.checkoutPageInner} />
+      <div className={styles.couponDiscount}>Coupon Discount</div>
+      <div className={styles.div3}>0 %</div>
+      <div className={styles.checkoutPageChild1} />
+      <div className={styles.tax}>TAX</div>
+      <div className={styles.div4}>5</div>
+      <div className={styles.total}>Total</div>
+      <div className={styles.div5}>$56.38</div>
+      <img className={styles.lineIcon} alt="" src="/line-26@2x.png" />
+    </div>
+  );
+};
+export default CheckoutPage;
