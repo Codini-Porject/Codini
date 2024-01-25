46e40595ebc788932d445ed00b47a16cf4c9c17e
louay <louaysakli65@gmail.com>
2024-01-25 09:08:19 +0100

    search functionality and the navbar

--- client/codini/app/Navbar/page.tsx
+++ client/codini/app/Navbar/page.tsx
@@ -4,7 +4,12 @@
 import Link from "next/link";
 import { useIdentity } from "../(auth)/IdentityContext";
 import "./navbar.css";
-import { Height } from "@mui/icons-material";
+
+
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar} from "@nextui-org/react";
+// import { AddNoteIcon } from "../addb/page";
+
+
 
 interface user {
   idstudent: number;
@@ -17,7 +22,7 @@ interface user {
 }
 const Navbar = () => {
   const { user } = useIdentity();
-
+  const iconClasses = "text-xl text-default-500 pointer-events-none flex-shrink-0";
   return (
     <nav className="Navbar">
       <img
@@ -31,9 +36,9 @@ const Navbar = () => {
             <div className="home">Home</div>
           </button>
         </Link>
-        <Link href="/">
+        <Link href="/learn">
           <button>
-            <div className="careers">Careers</div>
+            <div className="careers">Learn</div>
           </button>
         </Link>
         <Link href="/courses">
@@ -54,17 +59,38 @@ const Navbar = () => {
       </div>
 
       {user ? (
-        <div>
-          <Link href="/userProfile">
-            <img
-              className="lina rounded-full w-10 h-10 object-cover"
-              src={
-                user?.image ||
-                "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-              }
-              alt=""
-            />
-          </Link>
+        <div className="flex items-center gap-4 left-[100px] top-[100px] bg-red ">
+          <Dropdown className="border border p-4 rounded-lg bg-white shadow-md ">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="lina rounded-full w-20 h-20 object-cover"
+                src={
+                  user?.image ||
+                  "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+                }
+              />
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Profile Actions"
+              variant="flat"
+              className="py-2"
+            >
+              <DropdownItem key="profile" className="h-14 gap-1">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{user?.email}</p>
+              </DropdownItem>
+              
+                <DropdownItem key="settings" className="dropdown-item h-14 gap-2" shortcut="⌘" startContent={<Avatar showFallback />}>
+                <Link href="/userProfile" > My Profile</Link>
+                </DropdownItem>
+              
+              <DropdownItem key="logout" color="danger" className="dropdown-item" shortcut="⌘" startContent="">
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </div>
       ) : (
         <Link href="/login">
