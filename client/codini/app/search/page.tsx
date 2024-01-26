5c898c2f05ede761f185ec710d32950d4e9b388d
amineguezmir <guezmiramine1@gmail.com>
2024-01-26 11:37:43 +0100

    guezguez

--- client/codini/app/search/page.tsx
+++ client/codini/app/search/page.tsx
@@ -43,6 +43,7 @@ const Search: NextPage = () =>{
   }, []);
 
   useEffect(() => {
+   
     const filteredResults = teacherData.filter(item =>
       item.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
